3bac37bff93e4ae5187f049a28b296da27ce3aa4
ramza007 <ramzalvo007@gmail.com>
2018-02-20 15:49:04 +0300

    updated user interface

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 });
 
 var leapYear = function (year) {
-    if ((year % 4 === 0) && (year % 100 !== 0)) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
         return true;
     } else {
         return false;
@@ -16,3 +16,31 @@ var leapYear = function (year) {
 };
 
   // jQuery user interface logic here.
+
+// business logic
+var leapYear = function (year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// user interface logic
+$(document).ready(function () {
+    $("form#leap-year").submit(function (event) {
+        event.preventDefault();
+        var year = parseInt($("input#year").val());
+        var result = leapYear(year);
+
+        $(".year").text(year);
+
+        if (!result) {                 // same as writing if (result === false)
+            $(".not").text("not");
+        } else {
+            $(".not").text("");
+        }
+
+        $("#result").show();
+    });
+});
